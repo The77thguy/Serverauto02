387790667b80bbe0842e3a701fea0a2d042d2bb7
dres0047@elev.techcollege.dk <070194dres>
2022-09-07 14:06:48 +0200

    Du kan nu vælge dit source directory, og du kan vælge backup type

--- Backupscript01/main.py
+++ Backupscript01/main.py
@@ -1,8 +1,17 @@
 import os
 
+SRC_Path = input('What Directory Do you Wish To backup?\n')
+Backup_Type = input('Which Backup Type Would you like to perform?\n'
+                    'Write 1 To Perform A Full Backup Of Your Selected Directory\n'
+                    'Write 2 To Perform An Differential Backup\n')
+
 dst = r"C:\DEST"
-src = r"C:\SRC"
+src = SRC_Path
 
 #de forskellige parametre efter XCOPY kan ændres alt efter hvordan man ønsker at tage sin backup
 #der findes officiel dokumentation på XCOPY kommandoen fra microsoft
-print (os.system("xcopy %s %s" % (src, dst)))
+
+if Backup_Type == "1":
+    print (os.system("xcopy %s %s" % (src, dst)))
+
+else: print("Does Not Compute Seymore")
